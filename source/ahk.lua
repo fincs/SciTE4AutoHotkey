fincs/SciTE4AutoHotkey

commit 32f1b1911195e3fb707a04cf97f9f37320b88227

Overhaul and simplify AutoHotkey v1/v2 settings, see details:
- ahk.properties completely rewritten
- ahk2.properties removed (merged into ahk.properties)
- platforms.properties reduced to a bare minimum (moved everything
  possible into ahk.properties)
- New version of the syntax highlighting style ABI (s4ahk.style=2)
- Keyword/AutoComplete lists can now be generated automatically using
  the AutoHotkey v1/v2 documentation
- Disabled calltip functionality due to SciTE's behaviour being too
  inflexible -- (to be replaced by a future/undetermined system)
- Other miscellaneous changes

Things not yet in this commit:

- SciTE/Scintilla/Lexilla patches (including new AHKv2 lexer)
- Script used to autogenerate keyword lists
- Style editor changes + new default styles
- Other miscellaneous things

source/ahk.lua
@@ -15,6 +15,11 @@ local prepared = false
 local bkall = {}
 local bkcur = nil
 
+local lexerIgnoreStyles = {
+	[SCLEX_AHK1] = {SCE_AHK1_COMMENTLINE, SCE_AHK1_COMMENTBLOCK, SCE_AHK1_STRING, SCE_AHK1_ERROR, SCE_AHK1_ESCAPE, SCE_AHK1_LABEL},
+	[SCLEX_AHK2] = {SCE_AHK2_ERROR, SCE_AHK2_COMMENT_LINE, SCE_AHK2_COMMENT_BLOCK, SCE_AHK2_LABEL, SCE_AHK2_STRING, SCE_AHK2_ESCAPE},
+}
+
 -- ================================================== --
 -- OnClear event - fired when SciTE changes documents --
 -- ================================================== --
@@ -24,7 +29,7 @@ function OnClear()
 		-- Remove the current line markers.
 		ClearCurrentLineMarkers()
 	end
-	
+
 	SetMarkerColors()
 	editor.MarginSensitiveN[1] = true
 end
@@ -33,53 +38,32 @@ end
 -- OnChar event - needed by some features --
 -- ====================================== --
 
+function isAutoCompleteIgnoreException(ignoreStyles, curStyle)
+	-- Allow AutoComplete in variable %dereferences% (which are set to 'error'
+	-- when they are typed because of the missing closing percent sign)
+	return editor.Lexer == SCLEX_AHK1 and curStyle == SCE_AHK1_ERROR and
+		editor.CharAt[pos-1] == 37 and not isInTable(ignoreStyles, editor.StyleAt[pos-1])
+end
+
 function OnChar(curChar)
 	-- This function should only run when the Editor pane is focused.
 	if not editor.Focus then return false end
-	-- This function only works with the AutoHotkey v1 lexer
-	if not IsAHKv1() then return false end
-	
-	local ignoreStyles = {SCE_AHK1_COMMENTLINE, SCE_AHK1_COMMENTBLOCK, SCE_AHK1_STRING, SCE_AHK1_ERROR, SCE_AHK1_ESCAPE}
 
-	if curChar == "\n" then
-		local prevStyle = editor.StyleAt[getPrevLinePos()]
-		if not isInTable(ignoreStyles, prevStyle) then
-			return AutoIndent_OnNewLine()
-		end
-	elseif curChar == "{" then
-		local curStyle = editor.StyleAt[editor.CurrentPos-2]
-		if not isInTable(ignoreStyles, curStyle) then
-			AutoIndent_OnOpeningBrace()
-		end
-	elseif curChar == "}" then
-		local curStyle = editor.StyleAt[editor.CurrentPos-2]
-		if not isInTable(ignoreStyles, curStyle) then
-			AutoIndent_OnClosingBrace()
-		end
-	else
+	-- Cancel AutoComplete inside certain lexer-specific styles
+	local ignoreStyles = lexerIgnoreStyles[editor.Lexer]
+	if ignoreStyles ~= nil then
 		local curStyle = editor.StyleAt[editor.CurrentPos-2]
 		local pos = editor:WordStartPosition(editor.CurrentPos)
-		
-		-- Disable AutoComplete on comment/string/error/etc.
-		if isInTable(ignoreStyles, curStyle) then
-			-- ... but allow it in variable %dereferences% (which are set to 'error'
-			-- when they are typed because of the missing closing percent sign.
-			if curStyle == SCE_AHK1_ERROR and editor.CharAt[pos-1] == 37
-			  and not isInTable(ignoreStyles, editor.StyleAt[pos-1]) then
-				return false
+
+		if isInTable(ignoreStyles, curStyle) and not isAutoCompleteIgnoreException(ignoreStyles, curStyle) then
+			if editor:AutoCActive() then
+				editor:AutoCCancel()
 			end
-			return CancelAutoComplete()
+			return true
 		end
 	end
-	
-	return false
-end
 
-function CancelAutoComplete()
-	if editor:AutoCActive() then
-		editor:AutoCCancel()
-	end
-	return true
+	return AutoIndent_Main(curChar, ignoreStyles)
 end
 
 -- ================================================== --
@@ -125,7 +109,7 @@ end
 function OnMarginClick(position, margin)
 	-- This function only works with the AutoHotkey lexer
 	if not bkcur then return false end
-	
+
 	if margin ~= 1 then
 		return false
 	end
@@ -151,14 +135,10 @@ end
 -- OnDwellStart event - used to implement hovering --
 -- =============================================== --
 
-local NoDwellStyles = {
-	[SCLEX_AHK1] = {SCE_AHK1_COMMENTBLOCK, SCE_AHK1_COMMENTLINE, SCE_AHK1_STRING, SCE_AHK1_ESCAPE, SCE_AHK1_LABEL},
-	--[SCLEX_AHK2] = {SCE_AHK2_COMMENTBLOCK, SCE_AHK2_COMMENTLINE, SCE_AHK2_STRING, SCE_AHK2_ESCAPE, SCE_AHK2_LABEL},
-}
 function OnDwellStart(pos, word)
 	if not prepared then return end
 	if word ~= '' then
-		if isInTable(NoDwellStyles[editor.Lexer], editor.StyleAt[pos]) then
+		if isInTable(lexerIgnoreStyles[editor.Lexer], editor.StyleAt[pos]) then
 			return
 		end
 		local line, wordpos, wordlen = GetCurLineAndWordPos(pos)
@@ -176,12 +156,12 @@ end
 
 function DBGp_Connect()
 	if prepared then return end
-	
+
 	if localizewin("SciTEDebugStub") == false then
 		print("Window doesn't exist.")
 		return
 	end
-	
+
 	-- Initialize
 	pumpmsg(4112, 0, 0)
 	prepared = true
@@ -190,7 +170,7 @@ end
 
 function DBGp_BkReset()
 	if not bkcur then return end
-	
+
 	local bkstr = {}
 	for file, lines in pairs(bkall) do
 		local bklines = {}
@@ -199,7 +179,7 @@ function DBGp_BkReset()
 		end
 		table.insert(bkstr, file .. "|" .. table.concat(bklines, " "))
 	end
-	
+
 	-- Send all filenames|breakpoints as a single string
 	pumpmsgstr(4112, 5, table.concat(bkstr, "\n"))
 end
@@ -208,14 +188,14 @@ function DBGp_Disconnect()
 	-- Deinitialize
 	u = pumpmsg(4112, 255, 0)
 	if u == 0 then return false end
-	
+
 	prepared = false
 	ClearCurrentLineMarkers()
 end
 
 function DBGp_Inspect()
 	if not prepared then return end
-	
+
 	local word = editor:GetSelText()
 	if word == "" then
 		local line, wordpos, wordlen = GetCurLineAndWordPos(pos)
@@ -407,6 +387,29 @@ function isStartBlockStatement(line)
 		or isTryLine(line) or isCatchLineAllowBraces(line) or isFinallyAllowBraces(line)
 end
 
+function AutoIndent_Main(curChar, ignoreStyles)
+	if not IsAHKv1() then return false end
+
+	if curChar == "\n" then
+		local prevStyle = editor.StyleAt[getPrevLinePos()]
+		if not isInTable(ignoreStyles, prevStyle) then
+			return AutoIndent_OnNewLine()
+		end
+	elseif curChar == "{" then
+		local curStyle = editor.StyleAt[editor.CurrentPos-2]
+		if not isInTable(ignoreStyles, curStyle) then
+			AutoIndent_OnOpeningBrace()
+		end
+	elseif curChar == "}" then
+		local curStyle = editor.StyleAt[editor.CurrentPos-2]
+		if not isInTable(ignoreStyles, curStyle) then
+			AutoIndent_OnClosingBrace()
+		end
+	end
+
+	return false
+end
+
 -- This function is called when the user presses {Enter}
 function AutoIndent_OnNewLine()
 	local cmtLineStyle = SCE_AHK1_COMMENTLINE
@@ -416,9 +419,9 @@ function AutoIndent_OnNewLine()
 	local prevLine = GetFilteredLine(prevPos, cmtLineStyle, cmtBlockStyle)
 	local curPos = prevPos + 1
 	local curLine = editor:GetLine(curPos)
-	
+
 	if curLine ~= nil and string.find(curLine, "^%s*[^%s]+") then return end
-	
+
 	if isIndentStatement(prevLine) then
 		editor:Home()
 		editor:Tab()
@@ -445,12 +448,12 @@ function AutoIndent_OnOpeningBrace()
 	local prevPos = editor:LineFromPosition(editor.CurrentPos) - 1
 	local curPos = prevPos+1
 	if prevPos == -1 then return false end
-	
+
 	if editor.LineIndentation[curPos] == 0 then return false end
-	
+
 	local prevLine = GetFilteredLine(prevPos, cmtLineStyle, cmtBlockStyle)
 	local curLine = GetFilteredLine(curPos, cmtLineStyle, cmtBlockStyle)
-	
+
 	if string.find(curLine, "^%s*{%s*$") and isStartBlockStatement(prevLine)
 		and (editor.LineIndentation[curPos] > editor.LineIndentation[prevPos]) then
 		editor:Home()
@@ -468,10 +471,10 @@ function AutoIndent_OnClosingBrace()
 	local prevPos = curPos - 1
 	local prevprevPos = prevPos - 1
 	local secondChance = false
-	
+
 	if curPos == 0 then return false end
 	if editor.LineIndentation[curPos] == 0 then return false end
-	
+
 	if prevprevPos >= 0 then
 		local prevprevLine = GetFilteredLine(prevprevPos, cmtLineStyle, cmtBlockStyle)
 		local lowLvl = editor.LineIndentation[prevprevPos]
@@ -482,7 +485,7 @@ function AutoIndent_OnClosingBrace()
 			secondChance = true
 		end
 	end
-	
+
 	if string.find(curLine, "^%s*}%s*$") and (editor.LineIndentation[curPos] >= editor.LineIndentation[prevPos] or secondChance) then
 		editor:Home()
 		editor:BackTab()
@@ -497,12 +500,12 @@ end
 function OnBeforeSave(filename)
 	-- This function only works with the AutoHotkey lexer
 	if not InAHKLexer() then return false end
-	
+
 	if props['make.backup'] == "1" then
 		os.remove(filename .. ".bak")
 		os.rename(filename, filename .. ".bak")
 	end
-	
+
 	-- Also update breakpoints.  It's called from here and not OnSave
 	-- because OnBeforeSave is more reliable.  OnSave is not called if
 	-- the file is saved asynchronously and some other file is active
@@ -517,7 +520,7 @@ end
 function OpenInclude()
 	-- This function only works with the AutoHotkey lexer
 	if not InAHKLexer() then return false end
-	
+
 	local CurrentLine = editor:GetLine(editor:LineFromPosition(editor.CurrentPos))
 	if not string.find(CurrentLine, "^%s*%#[Ii][Nn][Cc][Ll][Uu][Dd][Ee]") then
 		print("Not an include line!")
@@ -538,19 +541,19 @@ function OpenInclude()
 	if cplace then
 		IncFile = string.sub(IncFile, 1, cplace-1)
 	end
-	
+
 	-- Delete spaces at the beginning and the end
 	IncFile = string.gsub(IncFile, "^%s*", "")
 	IncFile = string.gsub(IncFile, "%s*$", "")
-	
+
 	-- Replace variables
 	IncFile = string.gsub(IncFile, "%%[Aa]_[Ss][Cc][Rr][Ii][Pp][Tt][Dd][Ii][Rr]%%", props['FileDir'])
 	IncFile = string.gsub(IncFile, "%%[Aa]_[Ll][Ii][Nn][Ee][Ff][Ii][Ll][Ee]%%", props['FilePath'])
-	
+
 	a,b,IncLib = string.find(IncFile, "^<(.+)>$")
-	
+
 	if IncLib ~= nil then
-	
+
 		local IncLib2 = IncLib
 		local RawIncLib = IncLib
 		a,b,whatmatch = string.find(IncLib, "^(.-)_")
@@ -559,11 +562,11 @@ function OpenInclude()
 		end
 		IncLib = "\\"..IncLib..".ahk"
 		IncLib2 = "\\"..IncLib2..".ahk"
-		
+
 		local GlobalLib = props['AutoHotkeyDir'].."\\Lib"
 		local UserLib = props['SciteUserHome'].."\\..\\Lib"
 		local LocalLib = props['FileDir'].."\\Lib"
-		
+
 		for i,LibDir in ipairs({GlobalLib, UserLib, LocalLib}) do
 			if FileExists(LibDir..IncLib) then
 				scite.Open(LibDir..IncLib)
@@ -573,9 +576,9 @@ function OpenInclude()
 				return
 			end
 		end
-		
+
 		print("Library not found! Specified: '"..RawIncLib.."'")
-		
+
 	elseif FileExists(IncFile) then
 		scite.Open(IncFile)
 	else
@@ -631,12 +634,10 @@ function getPrevLinePos()
 	return linepos + string.len(linetxt) - 1
 end
 
-function isInTable(table, elem)
-	if table == null then return false end
-	for k,i in ipairs(table) do
-		if i == elem then
-			return true
-		end
+function isInTable(tbl, elem)
+	if tbl == nil then return false end
+	for i,v in ipairs(tbl) do
+		if v == elem then return true end
 	end
 	return false
 end

source/ahk.properties
@@ -6,12 +6,26 @@
 # write an analogous line in the User properties that sets it to blank.
 #
 
-# AutoHotkey path
-#potato.ignore
-AutoHotkeyDir=$(SciteDefaultHome)\..
+# Internal AutoHotkey path
 LocalAHK=$(SciteDefaultHome)\InternalAHK.exe
-#potato.endignore
-#potato.set AutoHotkeyDir=$(SciteDefaultHome)
+
+# AutoHotkey path
+AutoHotkeyDir=$(SciteDefaultHome)\..
+AutoHotkey=$(AutoHotkeyDir)\AutoHotkey.exe
+ahk.help.file=$(AutoHotkeyDir)\AutoHotkey.chm
+ahk.compiler=$(AutoHotkeyDir)\Compiler\Ahk2Exe.exe
+ahk.compiler.params=
+
+# AutoHotkey Lua editor script, needed for some features
+ext.lua.startup.script=$(SciteDefaultHome)\ahk.lua
+ext.lua.auto.reload=1
+
+# AutoHotkey debugger settings
+ahk.debugger.address=127.0.0.1
+ahk.debugger.port=9000
+ahk.debugger.capture.streams=1
+ahk.debugger.max.obj.children=100
+ahk.debugger.max.data=131072
 
 # Script file extensions
 ahk.file.extension=*.ahk
@@ -22,31 +36,58 @@ file.patterns.ahk=$(ahk.file.extension);$(ahkscriptlet.file.extension)
 filter.ahk=\
 AutoHotkey scripts (*.ahk)|$(ahk.file.extension)|\
 AHK scriptlets (*.scriptlet)|$(ahkscriptlet.file.extension)|
-lexer.$(file.patterns.ahk)=ahk1
-braces.ahk1.style=5
-braces.ahk1.style2=4
 
-# Autocomplete and calltip settings
-ahk.api=$(SciteDefaultHome)\ahk.api;$(SciteUserHome)\user.ahk.api
-api.$(file.patterns.ahk)=$(ahk.api)
-calltip.ahk1.use.escapes=1
-calltip.ahk1.word.characters=$(chars.alpha)$(chars.numeric)$(chars.accented)$_@#.
-calltip.ahk1.ignorecase=1
-calltip.ahk1.parameters.start= ,(
-calltip.ahk1.parameters.end=)
-calltip.ahk1.parameters.separator=,
-calltip.ahk1.skip.bare.words=1
-autocomplete.ahk1.ignorecase=1
-autocomplete.ahk1.start.characters=$(chars.alpha)$(chars.numeric)$(chars.accented)$_@#.
-word.characters.$(file.patterns.ahk)=$(chars.alpha)$(chars.numeric)$(chars.accented)$_@#
+# Placeholders for AutoHotkey v1/v2 platform selection
+file.patterns.ahk1=
+file.patterns.ahk2=
+
+# Lexer settings
+lexer.$(file.patterns.ahk1)=ahk1
+lexer.$(file.patterns.ahk2)=ahk2
+
+# Brace style settings
+braces.ahk1.style=5
+braces.ahk2.style=10
+
+# Word characters
+word.characters.$(file.patterns.ahk1)=$(chars.alpha)$(chars.numeric)$(chars.accented)$_@#
+word.characters.$(file.patterns.ahk2)=$(chars.alpha)$(chars.numeric)$(chars.accented)_
 
 # Abbreviation settings
 abbreviations.$(file.patterns.ahk)=$(SciteUserHome)\AhkAbbrevs.properties
 
-# Indentation settings
-indent.maintain.$(file.patterns.ahk)=0
+# Indentation settings (AutoHotkey v1)
+indent.maintain.$(file.patterns.ahk1)=1
 
-# Comment definitions
+# Indentation settings (AutoHotkey v2)
+statement.indent.$(file.patterns.ahk2)=6 catch else finally for if loop try while
+statement.lookback.$(file.patterns.ahk2)=2
+block.start.$(file.patterns.ahk2)=10 {
+block.end.$(file.patterns.ahk2)=10 }
+
+# Autocomplete settings (AutoHotkey v1)
+api.$(file.patterns.ahk1)=$(SciteDefaultHome)\ahk1.standard.api;$(SciteUserHome)\ahk1.user.api
+autocomplete.ahk1.ignorecase=1
+autocomplete.ahk1.start.characters=$(ahk1.word.characters).
+calltip.ahk1.ahk.mode=1
+calltip.ahk1.word.characters=
+calltip.ahk2.end.definition=
+calltip.ahk1.parameters.start=
+calltip.ahk1.parameters.separator=
+calltip.ahk1.parameters.end=
+
+# Autocomplete settings (AutoHotkey v2)
+api.$(file.patterns.ahk2)=$(SciteDefaultHome)\ahk2.standard.api;$(SciteUserHome)\ahk2.user.api
+autocomplete.ahk2.ignorecase=1
+autocomplete.ahk2.start.characters=$(ahk2.word.characters).
+calltip.ahk2.ahk.mode=1
+calltip.ahk2.word.characters=
+calltip.ahk2.end.definition=
+calltip.ahk2.parameters.start=
+calltip.ahk2.parameters.separator=
+calltip.ahk2.parameters.end=
+
+# Comment definitions (AutoHotkey v1)
 comment.block.ahk1=;~
 comment.stream.start.ahk1=/*
 comment.stream.end.ahk1=*/
@@ -54,51 +95,90 @@ comment.box.start.ahk1=/*
 comment.box.middle.ahk1= *
 comment.box.end.ahk1= */
 
-# Fold settings
-fold.at.else=1
+# Comment definitions (AutoHotkey v2)
+comment.block.ahk2=;~
+comment.stream.start.ahk2=/*
+comment.stream.end.ahk2=*/
+comment.box.start.ahk2=/*
+comment.box.middle.ahk2= *
+comment.box.end.ahk2= */
 
-# AutoHotkey Lua editor script, needed for some features
-ext.lua.startup.script=$(SciteDefaultHome)\ahk.lua
-ext.lua.auto.reload=1
+# Style definitions (AutoHotkey v1)
+style.ahk1.0=$(s4ahk.style.default)
+style.ahk1.1=$(s4ahk.style.comment.line)
+style.ahk1.2=$(s4ahk.style.comment.block)
+style.ahk1.3=$(s4ahk.style.escape)
+style.ahk1.4=$(s4ahk.style.old.synop)
+style.ahk1.5=$(s4ahk.style.operator)
+style.ahk1.6=$(s4ahk.style.string)
+style.ahk1.7=$(s4ahk.style.number)
+style.ahk1.8=$(s4ahk.style.ident.top)
+style.ahk1.9=$(s4ahk.style.old.deref)
+style.ahk1.10=$(s4ahk.style.label)
+style.ahk1.11=$(s4ahk.style.flow)
+style.ahk1.12=$(s4ahk.style.known.func)
+style.ahk1.13=$(s4ahk.style.known.class)
+style.ahk1.14=$(s4ahk.style.directive)
+style.ahk1.15=$(s4ahk.style.hotkey)
+style.ahk1.16=$(s4ahk.style.known.var)
+style.ahk1.17=$(s4ahk.style.ident.reserved)
+style.ahk1.18=$(s4ahk.style.old.user)
+style.ahk1.19=$(s4ahk.style.old.bivderef)
+style.ahk1.20=$(s4ahk.style.error)
 
-# Debugger settings
-ahk.debugger.address=127.0.0.1
-ahk.debugger.port=9000
-ahk.debugger.capture.streams=1
-ahk.debugger.max.obj.children=100
-ahk.debugger.max.data=131072
+# Keyword definitions (AutoHotkey v1)
+keywords.$(file.patterns.ahk1)=$(ahk1.keywords.flow)
+keywords2.$(file.patterns.ahk1)=$(ahk1.keywords.known.cmds)
+keywords3.$(file.patterns.ahk1)=$(ahk1.keywords.known.funcs)
+keywords4.$(file.patterns.ahk1)=$(ahk1.keywords.directives)
+keywords5.$(file.patterns.ahk1)=
+keywords6.$(file.patterns.ahk1)=$(ahk1.keywords.known.vars)
+keywords7.$(file.patterns.ahk1)=$(ahk1.keywords.reserved)
 
-# Old style format compatibility defines
-#potato.ignore
-s4ahk.style.default=$(style.ahk1.0)
-s4ahk.style.comment.line=$(style.ahk1.1)
-s4ahk.style.comment.block=$(style.ahk1.2)
-s4ahk.style.escape=$(style.ahk1.3)
-s4ahk.style.operator=$(style.ahk1.5)
-s4ahk.style.string=$(style.ahk1.6)
-s4ahk.style.number=$(style.ahk1.7)
-s4ahk.style.wordop=$(style.ahk1.17)
-s4ahk.style.var=$(style.ahk1.8)
-s4ahk.style.func=$(style.ahk1.13)
-s4ahk.style.directive=$(style.ahk1.14)
-s4ahk.style.label=$(style.ahk1.10)
-s4ahk.style.flow=$(style.ahk1.11)
-s4ahk.style.biv=$(style.ahk1.16)
-s4ahk.style.bif=$(style.ahk1.12)
-s4ahk.style.error=$(style.ahk1.20)
-#potato.endignore
-s4ahk.style.objprop=$(s4ahk.style.var),back:#F0FFF0
-s4ahk.style.objmethod=$(s4ahk.style.func),back:#F0FFF0
-s4ahk.style.biobjprop=$(s4ahk.style.biv),back:#F0FFF0
-s4ahk.style.biobjmethod=$(s4ahk.style.bif),back:#F0FFF0
+# Style definitions (AutoHotkey v2)
+style.ahk2.0=$(s4ahk.style.default)
+style.ahk2.1=$(s4ahk.style.error)
+style.ahk2.2=$(s4ahk.style.comment.line)
+style.ahk2.3=$(s4ahk.style.comment.block)
+style.ahk2.4=$(s4ahk.style.directive)
+style.ahk2.5=$(s4ahk.style.label)
+style.ahk2.6=$(s4ahk.style.flow)
+style.ahk2.7=$(s4ahk.style.number)
+style.ahk2.8=$(s4ahk.style.string)
+style.ahk2.9=$(s4ahk.style.escape)
+style.ahk2.10=$(s4ahk.style.operator)
+style.ahk2.11=$(s4ahk.style.ident.top)
+style.ahk2.12=$(s4ahk.style.ident.obj)
+style.ahk2.13=$(s4ahk.style.ident.reserved)
+
+# Top-level identifier substyles (AutoHotkey v2)
+substyles.ahk2.11=3
+style.ahk2.11.1=$(s4ahk.style.known.var)
+style.ahk2.11.2=$(s4ahk.style.known.func)
+style.ahk2.11.3=$(s4ahk.style.known.class)
+
+# Object identifier substyles (AutoHotkey v2)
+substyles.ahk2.12=2
+style.ahk2.12.1=$(s4ahk.style.known.obj.prop)
+style.ahk2.12.2=$(s4ahk.style.known.obj.method)
+
+# Keyword definitions (AutoHotkey v2)
+keywords.$(file.patterns.ahk2)=$(ahk2.keywords.directives.expr)
+keywords2.$(file.patterns.ahk2)=$(ahk2.keywords.directives.str)
+keywords3.$(file.patterns.ahk2)=$(ahk2.keywords.flow)
+keywords4.$(file.patterns.ahk2)=$(ahk2.keywords.reserved)
+substylewords.11.1.$(file.patterns.ahk2)=$(ahk2.keywords.known.vars)
+substylewords.11.2.$(file.patterns.ahk2)=$(ahk2.keywords.known.funcs)
+substylewords.11.3.$(file.patterns.ahk2)=$(ahk2.keywords.known.classes)
+substylewords.12.1.$(file.patterns.ahk2)=$(ahk2.keywords.known.props)
+substylewords.12.2.$(file.patterns.ahk2)=$(ahk2.keywords.known.methods)
 
 # Keywords
-import ahk.keywords
+import ahk1.keywords
+import ahk2.keywords
 
 # Editor commands
-#potato.ignore
 import ahk.commands
-#potato.endignore
 
 # TillaGoto settings
 import tillagoto

source/SciTEGlobal.properties
@@ -167,20 +167,45 @@ default.file.ext=.ahk
 open.filter=$(star filter.)All files (*.*)|*.*|
 save.filter=$(open.filter)
 
+# Default font configuration
+default.text.font=Consolas
+s4ahk.font.settings=font:$(default.text.font),size:11,weight:350
+
 # Global default styles for all languages
-default.text.font=Courier New
 # Default
-style.*.32=font:$(default.text.font),size:10
+style.*.32=$(s4ahk.font.settings),back:#FFFFFFF,fore:#000000
 # Line number
-style.*.33=fore:#111111,back:#DDDDDD,font:$(default.text.font),size:10
+style.*.33=fore:#111111,back:#DDDDDD
 # Brace highlight
 style.*.34=fore:#0000FF,bold
 # Brace incomplete highlight
-style.*.35=fore:#FF0000,bold 
+style.*.35=fore:#FF0000,bold
 # Control characters
 style.*.36=back:#00FF80
 # Indentation guides
 style.*.37=fore:#C0C0C0,back:#FFFFFF
+
+# Fallbacks for obsolete AHKv1 styles
+s4ahk.style.old.synop=$(s4ahk.style.operator)
+s4ahk.style.old.deref=$(s4ahk.style.var)
+s4ahk.style.old.user=$(s4ahk.style.var)
+s4ahk.style.old.bivderef=$(s4ahk.style.known.var)
+
+# Fallbacks for AHKv2 object identifier styles
+s4ahk.style.objprop=$(s4ahk.style.ident.top)
+s4ahk.style.biobjprop=$(s4ahk.style.known.var)
+s4ahk.style.biobjmethod=$(s4ahk.style.known.func)
+
+# Backwards compatibility with older styles
+s4ahk.style.hotkey=$(s4ahk.style.old.key)
+s4ahk.style.ident.top=$(s4ahk.style.var)
+s4ahk.style.ident.obj=$(s4ahk.style.objprop)
+s4ahk.style.ident.reserved=$(s4ahk.style.wordop)
+s4ahk.style.known.var=$(s4ahk.style.biv)
+s4ahk.style.known.func=$(s4ahk.style.bif)
+s4ahk.style.known.class=$(s4ahk.style.func)
+s4ahk.style.known.obj.prop=$(s4ahk.style.biobjprop)
+s4ahk.style.known.obj.method=$(s4ahk.style.biobjmethod)
 
 # Printing
 print.magnification=-1
@@ -263,7 +288,6 @@ automatic.updates=1
 
 # Import language properties files
 import ahk
-import ahk2
 import lua
 
 # Import other properties

source/ahk.commands.properties
@@ -14,10 +14,18 @@ Run selection|1123|\
 ||\
 Inspect variable...|1126|
 
-# Subsystem for debug command
+# Run (F5)
+command.go.$(file.patterns.ahk)="$(AutoHotkey)" /ErrorStdOut "$(FilePath)" $(1) $(2) $(3) $(4)
+
+# Debug (F7)
+command.build.$(file.patterns.ahk)="$(LocalAHK)" "$(SciteDefaultHome)\tools\SciTEDebug.ahk" "$(AutoHotkey)" $(1) $(2) $(3) $(4)
 command.build.subsystem.$(file.patterns.ahk)=2
 
-# Subsystem for help command
+# Compile (Ctrl+F7)
+command.compile.$(file.patterns.ahk)="$(ahk.compiler)" /in "$(FilePath)" $(ahk.compiler.params)
+
+# Help (F1)
+command.help.$(file.patterns.ahk)=$(CurrentWord)!$(ahk.help.file)
 command.help.subsystem.$(file.patterns.ahk)=4
 
 # ShellExecute/non-stream-capturing run command

commit 86a5c5902ef13c9e56d70605bebcecb09f0e04f8

Fix more breakage caused by 32f1b191

source/ahk.lua
@@ -38,7 +38,7 @@ end
 -- OnChar event - needed by some features --
 -- ====================================== --
 
-function isAutoCompleteIgnoreException(ignoreStyles, curStyle)
+function isAutoCompleteIgnoreException(ignoreStyles, curStyle, pos)
 	-- Allow AutoComplete in variable %dereferences% (which are set to 'error'
 	-- when they are typed because of the missing closing percent sign)
 	return editor.Lexer == SCLEX_AHK1 and curStyle == SCE_AHK1_ERROR and
@@ -55,7 +55,7 @@ function OnChar(curChar)
 		local curStyle = editor.StyleAt[editor.CurrentPos-2]
 		local pos = editor:WordStartPosition(editor.CurrentPos)
 
-		if isInTable(ignoreStyles, curStyle) and not isAutoCompleteIgnoreException(ignoreStyles, curStyle) then
+		if isInTable(ignoreStyles, curStyle) and not isAutoCompleteIgnoreException(ignoreStyles, curStyle, pos) then
 			if editor:AutoCActive() then
 				editor:AutoCCancel()
 			end

source/ahk.properties
@@ -58,9 +58,6 @@ word.characters.$(file.patterns.ahk2)=$(ahk2.word.characters)
 # Abbreviation settings
 abbreviations.$(file.patterns.ahk)=$(SciteUserHome)\AhkAbbrevs.properties
 
-# Indentation settings (AutoHotkey v1)
-indent.maintain.$(file.patterns.ahk1)=1
-
 # Indentation settings (AutoHotkey v2)
 statement.indent.$(file.patterns.ahk2)=6 catch else finally for if loop try while
 statement.lookback.$(file.patterns.ahk2)=2
@@ -71,25 +68,17 @@ block.end.$(file.patterns.ahk2)=10 }
 api.$(file.patterns.ahk1)=$(SciteDefaultHome)\ahk1.standard.api;$(SciteUserHome)\ahk1.user.api
 autocomplete.ahk1.ignorecase=1
 autocomplete.ahk1.start.characters=$(ahk1.word.characters).
+autocomplete.ahk1.typesep=?
 calltip.ahk1.ahk.mode=1
 calltip.ahk1.ignorecase=1
-calltip.ahk1.word.characters=
-calltip.ahk1.end.definition=
-calltip.ahk1.parameters.start=
-calltip.ahk1.parameters.separator=
-calltip.ahk1.parameters.end=
 
 # Autocomplete settings (AutoHotkey v2)
 api.$(file.patterns.ahk2)=$(SciteDefaultHome)\ahk2.standard.api;$(SciteUserHome)\ahk2.user.api
 autocomplete.ahk2.ignorecase=1
-autocomplete.ahk2.start.characters=$(ahk2.word.characters).
+autocomplete.ahk2.start.characters=$(ahk2.word.characters).#
+autocomplete.ahk2.typesep=?
 calltip.ahk2.ahk.mode=1
 calltip.ahk2.ignorecase=1
-calltip.ahk2.word.characters=
-calltip.ahk2.end.definition=
-calltip.ahk2.parameters.start=
-calltip.ahk2.parameters.separator=
-calltip.ahk2.parameters.end=
 
 # Comment definitions (AutoHotkey v1)
 comment.block.ahk1=;~
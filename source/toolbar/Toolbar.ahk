fincs/SciTE4AutoHotkey

commit 16b5e00dc8b9eda2faad45d30dd9c6958d3fae61

Add basic support for AutoHotkeyUX, see details:
- Platform properties have been removed in favor of smart autodetection
  logic written in script (PlatformRead.ahk -> PlatformDetect.ahk)
- Added support for the new AutoHotkey directory structure:
  - Side-by-side v1/v2 installs in subfolders named after their version
  - "Latest" v2 platforms (%AhkDir%\v2)
  - "Latest" v1.x platforms (%AhkDir%), backwards compatible with v1.x installer
  - "Default" entry:
    - If AutoHotkeyUX is present, this now defaults to v2 syntax highlighting
      and uses the AutoHotkey Launcher script instead of running an
      AutoHotkey executable directly within SciTE
    - Otherwise, %AhkDir%\AutoHotkey.exe is used (compatible with v1.x)
- Added support for variants with UI access (AutoHotkey*_UIA.exe)
- Old platform names migrated to new, verbose platform names

TODO:
- Add support for user-specified folders containing AutoHotkey executables
- Handle Ahk2Exe onboarding better (v1.1.34/v2+ uses .exe, older uses .bin)

source/toolbar/Toolbar.ahk
@@ -7,7 +7,7 @@
 #NoTrayIcon
 #SingleInstance Ignore
 #Include %A_ScriptDir%
-#Include PlatformRead.ahk
+#Include PlatformDetect.ahk
 #Include ComInterface.ahk
 #Include SciTEDirector.ahk
 #Include SciTEMacros.ahk
@@ -235,7 +235,7 @@ Loop, Parse, ToolbarProps, `n, `r
 		curtool.Picture := Trim(varz4)
 		curtool.IconNumber := 1
 	}
-	
+
 	_ToolButs .= curtool.Name "," (i ++) ",,autosize`n"
 	IL_Add(_ToolIL, curtool.Picture, curtool.IconNumber)
 }
@@ -282,9 +282,6 @@ Menu, ToolMenu, Add
 Menu, ToolMenu, Add, Open Global toolbar properties, editglobalprops
 Menu, ToolMenu, Add, Open Global autorun script, editglobalautorun
 Menu, ToolMenu, Add
-Menu, ToolMenu, Add, Open platform properties, editplatforms
-Menu, ToolMenu, Add, Reload platforms, reloadplatforms
-Menu, ToolMenu, Add
 Menu, ToolMenu, Add, Reload toolbar, reloadtoolbar
 Menu, ToolMenu, Add, Reload toolbar (with autorun), reloadtoolbarautorun
 Menu, ToolMenu, Add
@@ -326,13 +323,28 @@ if DirectorReady && !IsPortable
 Macro_Init()
 
 ; Initialize the platforms
-platforms := Util_ParsePlatforms("platforms.properties", platlist)
+global g_Platforms := Plat_DetectAll()
+curplatform := ""
 IfExist, %LocalSciTEPath%\_platform.properties
 {
 	FileReadLine, ov, %LocalSciTEPath%\_platform.properties, 2
 	curplatform := SubStr(ov, 14)
-}else
-	curplatform = Default
+}
+
+if !g_Platforms.HasKey(curplatform) {
+	; Convert old platform names to new names
+	switch curplatform {
+		case "ANSI":    curplatform := "Latest v1.1 (32-bit ANSI)"
+		case "Unicode": curplatform := "Latest v1.1 (32-bit Unicode)"
+		case "x64":     curplatform := "Latest v1.1 (64-bit Unicode)"
+		case "v2(x86)": curplatform := "Latest v2 (32-bit)"
+		case "v2(x64)": curplatform := "Latest v2 (64-bit)"
+	}
+
+	if !g_Platforms.HasKey(curplatform) {
+		curplatform := "Default"
+	}
+}
 
 Util_PopulatePlatformsMenu()
 
@@ -425,16 +437,6 @@ editglobalautorun:
 Run, SciTE.exe "%SciTEDir%\tools\Autorun.ahk"
 return
 
-editplatforms:
-Run, SciTE.exe "%SciTEDir%\platforms.properties"
-return
-
-reloadplatforms:
-Menu, PlatformMenu, DeleteAll
-platforms := Util_ParsePlatforms("platforms.properties", platlist)
-Util_PopulatePlatformsMenu()
-goto changeplatform
-
 reloadtoolbar:
 Director_Send("closing:")
 Msg_Reload()
@@ -472,12 +474,16 @@ return
 platswitch:
 curplatform := A_ThisMenuItem
 platswitch2:
-for i,plat in platlist
-	Menu, PlatformMenu, Uncheck, %plat%
-Menu, PlatformMenu, Check, %curplatform%
+for plat in g_Platforms
+{
+	if (plat == curplatform)
+		Menu, PlatformMenu, Check, %A_Index%&
+	else
+		Menu, PlatformMenu, Uncheck, %A_Index%&
+}
 changeplatform:
 FileDelete, %LocalSciTEPath%\_platform.properties
-FileAppend, % platforms[curplatform], %LocalSciTEPath%\_platform.properties
+FileAppend, % g_Platforms[curplatform], %LocalSciTEPath%\_platform.properties
 SendMessage, 1024+1, 0, 0,, ahk_id %scitehwnd%
 if DirectorReady
 	CurAhkExe := CoI.ResolveProp("AutoHotkey")
@@ -623,7 +629,7 @@ _ReloadWithAutoRun()
 GetSciTEOpenedFile()
 {
 	global scitehwnd, DirectorReady
-	
+
 	if DirectorReady
 		return Director_Send("askfilename:", true).value
 	else
@@ -651,7 +657,7 @@ ParseCmdLine(cmdline)
 {
 	global _IconLib, curplatform, LocalSciTEPath, SciTEDir, CurAhkExe
 	a := GetSciTEOpenedFile()
-	
+
 	StringReplace, cmdline, cmdline, `%FILENAME`%, % GetFilename(a), All
 	StringReplace, cmdline, cmdline, `%FILEPATH`%, % GetPath(a), All
 	StringReplace, cmdline, cmdline, `%FULLFILENAME`%, % a, All
@@ -667,13 +673,13 @@ ParseCmdLine(cmdline)
 
 Util_PopulatePlatformsMenu()
 {
-	global platlist, curplatform
-	
-	for i,plat in platlist
+	global curplatform
+
+	for plat in g_Platforms
 	{
 		Menu, PlatformMenu, Add, %plat%, platswitch
-		if (plat = curplatform)
-			Menu, PlatformMenu, Check, %plat%
+		if (plat == curplatform)
+			Menu, PlatformMenu, Check, %A_Index%&
 	}
 }
 

source/ahk.commands.properties
@@ -21,7 +21,7 @@ F11|1130|\
 Shift+F11|1131|
 
 # Run (F5)
-command.go.$(file.patterns.ahk)="$(AutoHotkey)" /ErrorStdOut "$(FilePath)" $(1) $(2) $(3) $(4)
+command.go.$(file.patterns.ahk)=$(ahk.launcher) /ErrorStdOut "$(FilePath)" $(1) $(2) $(3) $(4)
 
 # Debug (F7)
 command.build.$(file.patterns.ahk)="$(LocalAHK)" "$(SciteDefaultHome)\tools\SciTEDebug.ahk" "$(AutoHotkey)" $(1) $(2) $(3) $(4)

source/ahk.properties
@@ -12,6 +12,7 @@ LocalAHK=$(SciteDefaultHome)\InternalAHK.exe
 # AutoHotkey path
 AutoHotkeyDir=$(SciteDefaultHome)\..
 AutoHotkey=$(AutoHotkeyDir)\AutoHotkey.exe
+ahk.launcher="$(AutoHotkey)"
 ahk.help.file=$(AutoHotkeyDir)\AutoHotkey.chm
 ahk.compiler=$(AutoHotkeyDir)\Compiler\Ahk2Exe.exe
 ahk.compiler.params=

source/platforms.properties
@@ -1,46 +1,0 @@
-; SciTE settings for AutoHotkey platforms
-;
-; Do NOT edit this file!
-;
-
-;---- Platform detection ----
-
-.platform Default
-.condplat ANSI    FileExist, %AhkDir%\AutoHotkeyA32.exe
-.condplat Unicode FileExist, %AhkDir%\AutoHotkeyU32.exe
-.condplat x64     FileExist, %AhkDir%\AutoHotkeyU64.exe
-.condplat v2(x86) FileExist, %AhkDir%\v2\AutoHotkey32.exe
-.condplat v2(x64) FileExist, %AhkDir%\v2\AutoHotkey64.exe
-
-.if Default, ANSI, Unicode, x64
-	ahk.version=1
-	file.patterns.ahk1=$(file.patterns.ahk)
-.end
-
-.if v2(x86), v2(x64)
-	ahk.version=2
-	file.patterns.ahk2=$(file.patterns.ahk)
-.end
-
-;---- Path overrides ----
-
-.if ANSI, Unicode, x64
-	.if ANSI    AutoHotkeySuffix=A32
-	.if ANSI    AutoHotkeyPrettyName=ANSI 32-bit
-	.if Unicode AutoHotkeySuffix=U32
-	.if Unicode AutoHotkeyPrettyName=Unicode 32-bit
-	.if x64     AutoHotkeySuffix=U64
-	.if x64     AutoHotkeyPrettyName=Unicode 64-bit
-
-	AutoHotkey=$(AutoHotkeyDir)\AutoHotkey$(AutoHotkeySuffix).exe
-	ahk.compiler.params=/bin "$(AutoHotkeyDir)\Compiler\$(AutoHotkeyPrettyName).bin"
-.end
-
-.if v2(x86), v2(x64)
-	AutoHotkeyDir_v2=$(AutoHotkeyDir)\v2
-	.if v2(x86) AutoHotkey=$(AutoHotkeyDir_v2)\AutoHotkey32.exe
-	.if v2(x64) AutoHotkey=$(AutoHotkeyDir_v2)\AutoHotkey64.exe
-
-	ahk.compiler.params=/base "$(AutoHotkey)"
-	ahk.help.file=$(AutoHotkeyDir_v2)\AutoHotkey.chm
-.end

source/toolbar/PlatformDetect.ahk
@@ -1,0 +1,97 @@
+
+Plat_DetectAll() {
+	global AhkDir
+
+	plats := {}
+
+	Plat_OnBoardDefault(plats, AhkDir, "Default")
+	Plat_OnBoardV1(plats, AhkDir, "Latest v1.1")
+
+	if InStr(FileExist(AhkDir "\v2"), "D") {
+		Plat_OnBoardV2(plats, AhkDir "\v2", "Latest v2")
+	}
+
+	Loop Files, %AhkDir%\*.*, D
+	{
+		if RegExMatch(A_LoopFileName, "^v([12])\.", o) {
+			if (o1 == "1")
+				Plat_OnBoardV1(plats, A_LoopFileLongPath, A_LoopFileName)
+			else if (o1 == "2")
+				Plat_OnBoardV2(plats, A_LoopFileLongPath, A_LoopFileName)
+		}
+	}
+
+	return plats
+}
+
+Plat_OnBoardDefault(plats, path, name) {
+	ahkver := 1
+
+	if Util_FileExistProperly(exe := path "\UX\AutoHotkeyUX.exe") {
+		ahkver := 2
+	}
+
+	platdata := "# THIS FILE IS SCRIPT-GENERATED - DON'T TOUCH`n"
+	platdata .= "ahk.platform=" name "`n"
+	platdata .= "ahk.version=" ahkver "`n"
+	platdata .= "file.patterns.ahk" ahkver "=$(file.patterns.ahk)`n"
+
+	if (ahkver == 2) {
+		platdata .= "ahk.launcher=""" exe """ """ path "\UX\launcher.ahk""`n"
+
+		if Util_FileExistProperly(helpfile := path "\v2\AutoHotkey.chm")
+			platdata .= "ahk.help.file=" helpfile "`n"
+	}
+
+	plats[name] := platdata
+}
+
+Plat_OnBoardV1(plats, path, name) {
+	if A_Is64bitOS {
+		Plat_TryAdd(plats, path "\AutoHotkeyU64.exe",     name " (64-bit Unicode)", 1)
+		Plat_TryAdd(plats, path "\AutoHotkeyU64_UIA.exe", name " (64-bit Unicode; UI access)", 1)
+	}
+	Plat_TryAdd(plats, path "\AutoHotkeyU32.exe",     name " (32-bit Unicode)", 1)
+	Plat_TryAdd(plats, path "\AutoHotkeyU32_UIA.exe", name " (32-bit Unicode; UI access)", 1)
+	Plat_TryAdd(plats, path "\AutoHotkeyA32.exe",     name " (32-bit ANSI)", 1)
+	Plat_TryAdd(plats, path "\AutoHotkeyA32_UIA.exe", name " (32-bit ANSI; UI access)", 1)
+}
+
+Plat_OnBoardV2(plats, path, name) {
+	if A_Is64bitOS {
+		Plat_TryAdd(plats, path "\AutoHotkey64.exe",     name " (64-bit)", 2)
+		Plat_TryAdd(plats, path "\AutoHotkey64_UIA.exe", name " (64-bit; UI access)", 2)
+	}
+	Plat_TryAdd(plats, path "\AutoHotkey32.exe",     name " (32-bit)", 2)
+	Plat_TryAdd(plats, path "\AutoHotkey32_UIA.exe", name " (32-bit; UI access)", 2)
+}
+
+Util_FileExistProperly(path) {
+	attrib := DllCall("GetFileAttributes", "str", path, "uint")
+	return !(attrib == 0xffffffff) and !(attrib & 0x410)
+}
+
+Plat_TryAdd(plats, exe, name, ahkver) {
+	if not Util_FileExistProperly(exe)
+		return
+
+	verSize := DllCall("version\GetFileVersionInfoSize", "str", exe, "uint*", 0, "uint")
+	if not verSize
+		return
+
+	VarSetCapacity(verInfo, verSize)
+	if not DllCall("version\GetFileVersionInfo", "str", exe, "uint", 0, "uint", verSize, "ptr", &verInfo)
+		return
+
+	platdata := "# THIS FILE IS SCRIPT-GENERATED - DON'T TOUCH`n"
+	platdata .= "ahk.platform=" name "`n"
+	platdata .= "ahk.version=" ahkver "`n"
+	platdata .= "file.patterns.ahk" ahkver "=$(file.patterns.ahk)`n"
+	platdata .= "AutoHotkey=" exe "`n"
+
+	SplitPath path,, pathdir
+	if Util_FileExistProperly(helpfile := pathdir "\AutoHotkey.chm")
+		platdata .= "ahk.help.file=" helpfile "`n"
+
+	plats[name] := platdata
+}

source/toolbar/PlatformRead.ahk
@@ -1,114 +1,0 @@
-;
-; File encoding:  UTF-8
-;
-
-Util_ParsePlatforms(file, ByRef platlist)
-{
-	plats := {}
-	FileRead, file, %file%
-	
-	platlist := Util_EnumPlatforms(file)
-	for i,plat in platlist
-		plats[plat] := Util_EvalPlatform(file, plat)
-	
-	return plats
-}
-
-Util_EnumPlatforms(ByRef file)
-{
-	plats := {}
-	Loop, Parse, file, `n, `r
-	{
-		line := Trim(A_LoopField)
-		if (l := SubStr(line, 1, 10)) = ".platform "
-		{
-			lplats := SubStr(line, 11)
-			Loop, Parse, lplats, `,
-				plats.Insert(Trim(A_LoopField))
-		} else if (l = ".condplat ")
-		{
-			StringTrimLeft, line, line, 10
-			if RegExMatch(line, "^\s*(.+?)\s+(.+)$", o)
-			{
-				res := false
-				,plat := o1
-				,cond := o2
-				,parm := {}
-				Loop, Parse, cond, `,
-					(A_Index > 1) ? parm.Insert(Util_Dereference(Trim(A_LoopField))) : (func := Trim(A_LoopField))
-				if func in FileExist,WinActive,WinExist
-					res := !!%func%(parm*)
-				if res
-					plats.Insert(plat)
-			}
-		}
-	}
-	return plats
-}
-
-Util_Dereference(str)
-{
-	global AhkDir
-	StringReplace, str, str, `%AhkDir`%, %AhkDir%, All
-	StringReplace, str, str, ```%, `%, All
-	return str
-}
-
-Util_EvalPlatform(ByRef file, plat)
-{
-	evalresult = # THIS FILE IS SCRIPT-GENERATED - DON'T TOUCH`nahk.platform=%plat%`n
-	condlock = 0
-	Loop, Parse, file, `n, `r
-	{
-		line := Trim(A_LoopField)
-		
-		; Minimal parsing when in conditional section
-		if condlock > 0
-		{
-			if SubStr(line, 1, 4) = ".if " && !InStr(line, "=")
-				condlock ++
-			if line = .end
-				condlock --
-			continue
-		}
-		
-		; Comment ignoring
-		else if SubStr(line, 1, 1) = ";"
-			continue
-		
-		; Conditional parsing
-		else if SubStr(line, 1, 4) = ".if "
-		{
-			isAssign := false
-			isTrue := false
-			
-			plats := Trim(SubStr(line, 5))
-			if InStr(plats, "=")
-			{
-				isAssign := true
-				spcPos := InStr(plats, " ")
-				line := Trim(SubStr(plats, spcPos+1))
-				plats := SubStr(plats, 1, spcPos-1)
-			}
-			
-			Loop, Parse, plats, `,
-			if(Trim(A_LoopField) = plat)
-			{
-				isTrue := true
-				break
-			}
-			
-			if(!isTrue && !isAssign)
-				condlock ++
-			else if(isTrue && isAssign)
-				evalresult .= line "`n"
-		}
-		
-		; Statement
-		else if InStr(line, "=")
-			evalresult .= line "`n"
-	}
-	
-	StringTrimRight, evalresult, evalresult, 1
-	return evalresult
-}

source/tools/Lib/AHKType.ahk
@@ -1,44 +1,0 @@
-
-AHKType(exeName)
-{
-	FileGetVersion, vert, %exeName%
-	if !vert
-		return "FAIL"
-	
-	StringSplit, vert, vert, .
-	vert := vert4 | (vert3 << 8) | (vert2 << 16) | (vert1 << 24)
-	
-	exeMachine := GetExeMachine(exeName)
-	if !exeMachine
-		return "FAIL"
-	
-	if (exeMachine != 0x014C) && (exeMachine != 0x8664)
-		return "FAIL"
-	
-	if !(VersionInfoSize := DllCall("version\GetFileVersionInfoSize", "str", exeName, "uint*", null, "uint"))
-		return "FAIL"
-	
-	VarSetCapacity(VersionInfo, VersionInfoSize)
-	if !DllCall("version\GetFileVersionInfo", "str", exeName, "uint", 0, "uint", VersionInfoSize, "ptr", &VersionInfo)
-		return "FAIL"
-	
-	if !DllCall("version\VerQueryValue", "ptr", &VersionInfo, "str", "\VarFileInfo\Translation", "ptr*", lpTranslate, "uint*", cbTranslate)
-		return "FAIL"
-	
-	oldFmt := A_FormatInteger
-	SetFormat, IntegerFast, H
-	wLanguage := NumGet(lpTranslate+0, "UShort")
-	wCodePage := NumGet(lpTranslate+2, "UShort")
-	id := SubStr("0000" SubStr(wLanguage, 3), -3, 4) SubStr("0000" SubStr(wCodePage, 3), -3, 4)
-	SetFormat, IntegerFast, %oldFmt%
-
-	if !DllCall("version\VerQueryValue", "ptr", &VersionInfo, "str", "\StringFileInfo\" id "\ProductName", "ptr*", pField, "uint*", cbField)
-		return "FAIL"
-	
-	; Check it is actually an AutoHotkey executable
-	if !InStr(StrGet(pField, cbField), "AutoHotkey")
-		return "FAIL"
-	
-	; We're dealing with a legacy version if it's prior to v1.1
-	return vert >= 0x01010000 ? "Modern" : "Legacy"
-}

source/tools/SciTEDebug.ahk
@@ -19,10 +19,6 @@ ATM_STOPDEBUG  := ATM_OFFSET+1
 ATM_DRUNTOGGLE := ATM_OFFSET+4
 SciControl_InitConstants()
 
-global A_Args := []
-Loop, %0%
-	A_Args.Push(%A_Index%)
-
 if A_IsCompiled
 {
 	MsgBox, 16, %g_appTitle%, This program *must* be a uncompiled script!
@@ -54,34 +50,7 @@ global dbgMaxData := oSciTE.ResolveProp("ahk.debugger.max.data")+0
 if A_Args[1] = "/attach"
 	bIsAttach := true
 else
-{
-	AhkExecutable := A_Args.RemoveAt(1)
-	IfNotExist, %AhkExecutable%
-	{
-		MsgBox, 16, %g_appTitle%, The AutoHotkey executable doesn't exist!
-		ExitApp
-	}
-
-	Loop, Files, %AhkExecutable%
-	{
-		AhkExecutable := A_LoopFileLongPath
-		break
-	}
-	
-	ahkType := AHKType(AhkExecutable)
-	
-	if ahkType = FAIL
-	{
-		MsgBox, 16, %g_appTitle%, Invalid AutoHotkey executable!
-		ExitApp
-	}
-	
-	if ahkType = Legacy
-	{
-		MsgBox, 16, %g_appTitle%, Debugging is not supported in legacy versions of AutoHotkey (prior to v1.1).
-		ExitApp
-	}
-}
+	AhkLauncher := oSciTE.ResolveProp("ahk.launcher")
 
 ; Get the HWND of SciTE and its Scintilla controls
 scitehwnd := oSciTE.SciTEHandle
@@ -166,7 +135,7 @@ SplitPath, szFilename,, szDir
 
 allArgs := ObjJoin(A_Args,, """")
 if !bIsAttach
-	Run, "%AhkExecutable%" /Debug=%dbgAddr%:%dbgPort% "%szFilename%" %allArgs%, %szDir%,, Dbg_PID ; run AutoHotkey and store its process ID
+	Run, %AhkLauncher% /Debug=%dbgAddr%:%dbgPort% "%szFilename%" %allArgs%, %szDir%,, Dbg_PID ; run AutoHotkey and store its process ID
 else
 {
 	; Set the Last Found Window

commit c58b359613b22249caa3fb14755abd3736a40a5b

Toolbar: fix typo introduced by 16b5e00dc

source/toolbar/Toolbar.ahk
@@ -349,7 +349,7 @@ if !g_Platforms.HasKey(curplatform) {
 Util_PopulatePlatformsMenu()
 
 FileRead, temp, %LocalSciTEPath%\_platform.properties
-if platforms[curplatform] != temp
+if g_Platforms[curplatform] != temp
 	gosub changeplatform
 
 if DirectorReady